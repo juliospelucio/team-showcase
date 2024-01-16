24e2d0009bed9d7fad0f6b9d53cef0a339518fba
Julio dos Santos Pelucio <julio.pelucio@matriz.grupoadrianocobuccio.local>
2024-01-15 23:11:18 -0300

    feat - added card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden">
+    <img
+      class="w-full h-48 object-cover"
+      src="@/assets/images/julio-draw.jpg"
+      alt="Profile Picture"
+    />
+    <div class="p-4">
+      <h2 class="text-xl font-semibold text-gray-800">Júlio dos Santos Pelúcio</h2>
+      <p class="text-gray-600">FullStack Developer</p>
+      <p class="text-gray-700 mt-2">Gosta de encher o saco dos seus colegas</p>
+      <div class="flex justify-end items-center mt-4">
+        <a href="#" class="text-blue-500 hover:underline">Ver perfil</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import CardComponent from '@/components/CardComponent.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="bg-gray-100 min-h-screen flex flex-wrap items-center justify-around">
+    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
+    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
+    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
+    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
+    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
+    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
+    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
+  </div>
 </template>
+
+<style scoped></style>
