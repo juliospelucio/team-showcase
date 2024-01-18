dc24012fa3c6d1fe3c89ca01ed626ed345442324
Julio dos Santos Pelucio <julio.pelucio@matriz.grupoadrianocobuccio.local>
2024-01-17 23:43:17 -0300

    feat - backend integration

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,18 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+
+export interface Props {
+  photo: string
+  name: string
+  role: string
+  info: string
+}
+
+const props = defineProps<Props>()
+
+const imagePath = computed(() => new URL('/src/assets/images/' + props.photo, import.meta.url).href)
+</script>
 
 <template>
-  <div class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden">
-    <img
-      class="w-full h-48 object-cover"
-      src="@/assets/images/julio-draw.jpg"
-      alt="Profile Picture"
-    />
+  <div
+    class="max-w-sm min-w-72 bg-white shadow-lg rounded-md overflow-hidden border border-orange-600"
+  >
+    <div class="w-full bg-orange-500">
+      <img class="w-full h-48 object-cover px-[0.10rem]" :src="imagePath" alt="Profile Picture" />
+    </div>
     <div class="p-4">
-      <h2 class="text-xl font-semibold text-gray-800">Júlio dos Santos Pelúcio</h2>
-      <p class="text-gray-600">FullStack Developer</p>
-      <p class="text-gray-700 mt-2">Gosta de encher o saco dos seus colegas</p>
+      <h2 class="text-xl font-semibold text-gray-800">{{ props.name }}</h2>
+      <p class="text-gray-600">{{ props.role }}</p>
+      <p class="text-gray-700 mt-2">{{ props.info }}</p>
       <div class="flex justify-end items-center mt-4">
-        <a href="#" class="text-blue-500 hover:underline">Ver perfil</a>
+        <a href="#" class="text-orange-500 hover:underline">Go to Profile</a>
       </div>
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
 import CardComponent from '@/components/CardComponent.vue'
+import api from '@/services/api'
+import { ref, onMounted } from 'vue'
+
+interface User {
+  id: string
+  photo: string
+  name: string
+  role: string
+  info: string
+}
+
+const users = ref<User[]>([])
+
+onMounted(async () => {
+  try {
+    const res = await api.get(`/users`)
+    users.value = res.data
+  } catch (error) {
+    console.log(error)
+  }
+})
 </script>
 
 <template>
-  <div class="bg-gray-100 min-h-screen flex flex-wrap items-center justify-around">
-    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
-    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
-    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
-    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
-    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
-    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
-    <CardComponent class="max-w-sm bg-white shadow-lg rounded-md overflow-hidden mb-4" />
+  <div class="bg-gray-100 min-h-screen flex flex-wrap gap-1 items-center justify-around px-5">
+    <CardComponent
+      v-for="user in users"
+      :key="user.id"
+      :photo="user.photo"
+      :name="user.name"
+      :role="user.role"
+      :info="user.info"
+    />
   </div>
 </template>
 
