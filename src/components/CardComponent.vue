4e4e7f5bbffa1ea2f707ed14fdd2cb6dc988ecf0
Julio dos Santos Pelucio <julio.pelucio@matriz.grupoadrianocobuccio.local>
2024-01-22 21:56:43 -0300

    feat - added favicon and card image adjusts

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -14,18 +14,21 @@ const imagePath = computed(() => new URL('/src/assets/images/' + props.photo, im
 </script>
 
 <template>
-  <div
-    class="max-w-72 min-w-72 bg-white shadow-lg rounded-md overflow-hidden border border-orange-600"
-  >
+  <div class="w-64 h-96 bg-white shadow-lg rounded-md overflow-hidden border border-orange-600">
     <div class="w-full bg-orange-500">
       <img class="w-full h-52 object-cover px-[0.10rem]" :src="imagePath" alt="Profile Picture" />
     </div>
     <div class="p-4">
       <h2 class="text-xl font-semibold text-gray-800">{{ props.name }}</h2>
       <p class="text-gray-600">{{ props.role }}</p>
-      <p class="text-gray-700 mt-2">{{ props.info }}</p>
-      <div class="flex justify-end items-center mt-4">
-        <a href="#" class="text-orange-500 hover:underline">Go to Profile</a>
+      <p class="text-gray-700 mt-2 truncate cursor-help" :title="props.info">{{ props.info }}</p>
+      <div class="flex justify-between items-center mt-4">
+        <a href="#" class="text-blue-500 hover:underline">View Profile</a>
+        <div class="flex items-center">
+          <img class="w-8 h-8 rounded-full mr-2 border border-orange-600" src="@/assets/images/team-logo.png"
+            alt="Avatar">
+          <span class="text-gray-700">Foxtsune</span>
+        </div>
       </div>
     </div>
   </div>
