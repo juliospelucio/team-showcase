28354145e3218f1dd122e6a5bc7a4dc1f1850319
Julio dos Santos Pelucio <julio.pelucio@matriz.grupoadrianocobuccio.local>
2024-01-26 16:18:23 -0300

    feat - added timeline item component

---
+++ src/components/TimeLineItemComponent.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+export interface Props {
+  dates: RelevantDate[]
+}
+
+interface RelevantDate {
+  description: string
+  date: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  dates() {
+    return [
+      {
+        description: '',
+        date: ''
+      }
+    ]
+  }
+})
+</script>
+
+<template>
+  <div
+    v-for="date in props.dates"
+    class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group"
+  >
+    <div
+      class="flex justify-center items-center w-24 h-24 text-white font-medium bg-orange-500 rounded-full border border-white shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"
+    >
+      {{ date.date }}
+    </div>
+
+    <div
+      class="text-center w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-orange-200 shadow"
+    >
+      {{ date.description }}
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import TimeLineItemComponent from '@/components/TimeLineItemComponent.vue'
 import CheckIcon from '@/components/icons/CheckIcon.vue'
 import FacebookIcon from '@/components/icons/FacebookIcon.vue'
 import InstagramIcon from '@/components/icons/InstagramIcon.vue'
@@ -19,10 +20,10 @@ interface User {
   linkedin: string
   likes: string[]
   dislikes: string[]
-  relevant_dates: SignificanteDates[]
+  relevant_dates: RelevantDates[]
 }
 
-interface SignificanteDates {
+interface RelevantDates {
   description: string
   date: string
 }
@@ -95,27 +96,11 @@ onMounted(async () => {
     </div>
 
     <hr class="border border-orange-600 mt-5" />
-    <!-- TODO Add start date and end date -->
 
     <div
       class="mt-10 space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent"
     >
-      <div
-        v-for="date in user?.relevant_dates"
-        class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group"
-      >
-        <div
-          class="flex justify-center items-center w-24 h-24 text-white font-medium bg-orange-500 rounded-full border border-white shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"
-        >
-          {{ date.date }}
-        </div>
-
-        <div
-          class="text-center w-[calc(100%-4rem)] md:w-[calc(50%-2.5rem)] bg-white p-4 rounded border border-orange-200 shadow"
-        >
-          {{ date.description }}
-        </div>
-      </div>
+      <TimeLineItemComponent :dates="user?.relevant_dates" />
     </div>
   </div>
 </template>
